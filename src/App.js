f62a7fb76bca3962d5aba84d9e4f1ed39fa7d4b6
unknown <truongluan280702@gmail.com>
2022-09-23 09:39:34 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          hehloo
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
